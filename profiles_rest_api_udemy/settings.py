788e3edbf20bb177546fb80723407eee4bc02c5a
shamansk1y <shamansky1225@gmail.com>
2023-05-10 20:45:07 +0200

    done

--- profiles_rest_api_udemy/settings.py
+++ profiles_rest_api_udemy/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'ec2-16-16-219-117.eu-north-1.compute.amazonaws.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
